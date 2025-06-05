9852f83b784b0d13db65013250868136a6b8464d
vitorcosta880 <vidurieuxc@gmail.com>
2025-06-05 20:56:22 -0300

    banco.sql

--- banco.sql
+++ banco.sql
@@ -1,21 +1,36 @@
-#criar o database chamado db_alunos
-create database if not exists db_alunos;
+create database if not exists controle_estoque;
+use controle_estoque;
 
-#entrar no database db_alunos
-use db_alunos;
-
-#remove a tabela para recriá-la
-drop table if exists tb_alunos;
-
-#cria a tabela de tb_alunos
-CREATE TABLE tb_alunos ( 
-    id      INTEGER NOT NULL, 
-    nome    VARCHAR(100), 
-    idade   INTEGER,
-    curso   VARCHAR(50),
-    fase    INTEGER,
-    PRIMARY KEY(id)
+create table categorias; (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    descricao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-#lista a tabela criada
-show tables;
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco_unitario DECIMAL(10,2) NOT NULL,
+    unidade VARCHAR(20) NOT NULL COMMENT 'Ex: un, kg, lt, m, etc',
+    quantidade_estoque INT DEFAULT 0,
+    quantidade_minima INT DEFAULT 0,
+    quantidade_maxima INT DEFAULT 0,
+    categoria_id INT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id));
+    CREATE TABLE fornecedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(20),
+    telefone VARCHAR(20),
+    email VARCHAR(100),
+    endereco TEXT
+    );
+    CREATE TABLE produto_fornecedor (
+    produto_id INT NOT NULL,
+    fornecedor_id INT NOT NULL,
+    PRIMARY KEY (produto_id, fornecedor_id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id) ON DELETE CASCADE,
+    FOREIGN KEY (fornecedor_id) REFERENCES fornecedores(id) ON DELETE CASCADE
+);
