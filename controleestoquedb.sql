4db3e439c908bb06d0d592630f6de8aa3b7baa57
vitorcosta880 <vidurieuxc@gmail.com>
2025-06-05 21:11:38 -0300

    Add files via upload

---
+++ controleestoquedb.sql
@@ -0,0 +1,37 @@
+create database if not exists controle_estoque;
+use controle_estoque;
+
+create table if not exists categorias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    descricao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE if not exists produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco_unitario DECIMAL(10,2) NOT NULL,
+    unidade VARCHAR(20) NOT NULL COMMENT 'Ex: un, kg, lt, m, etc',
+    quantidade_estoque INT DEFAULT 0,
+    quantidade_minima INT DEFAULT 0,
+    quantidade_maxima INT DEFAULT 0,
+    categoria_id INT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id));
+    CREATE TABLE fornecedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(20),
+    telefone VARCHAR(20),
+    email VARCHAR(100),
+    endereco TEXT
+    );
+    CREATE TABLE if not exists produto_fornecedor (
+    produto_id INT NOT NULL,
+    fornecedor_id INT NOT NULL,
+    PRIMARY KEY (produto_id, fornecedor_id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id) ON DELETE CASCADE,
+    FOREIGN KEY (fornecedor_id) REFERENCES fornecedores(id) ON DELETE CASCADE
+);
+select * from produto_fornecedor;
